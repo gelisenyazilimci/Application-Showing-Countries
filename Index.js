0fad973c2b78e09a9d4a77a104833c0a49d2096d
gelisenyazilimci <burbyets@gmail.com>
2025-01-14 18:08:25 +0300

    view of neighboring countries opened

--- Index.js
+++ Index.js
@@ -7,34 +7,46 @@ function displayCountry (country) {
 
     request.addEventListener('load', function () {
         // this.responseText --> Metin olarak görüntülenmesini sağlar.
-        const data = JSON.parse(this.responseText); // Gelen JSON veri tipini Javascript'in anlayacağı bir hale çevirir.
-        console.log(data);
-        setCountry(data[0]);
+        const data = JSON.parse(request.responseText); // Gelen JSON veri tipini Javascript'in anlayacağı bir hale çevirir.
+        setCountry(data);
+
+        // komsu country'ler
+        const neighborsRequest = new XMLHttpRequest();
+        const countries = data[0]["borders"].toString();
+        neighborsRequest.open('GET', `https://restcountries.com/v3.1/alpha?codes=${countries}`, true);
+        neighborsRequest.send();
+        neighborsRequest.addEventListener('load', function () {
+            const data = JSON.parse(neighborsRequest.responseText);
+            setCountry(data);
+        });
     });
 }
 function setCountry(data) {
-    const html =
-    `
-    <div class="col-3">
-        <div class="card h-100">
-            <img src="${data.flags["png"]}" class="card-img-top" alt="">
-                <div class="card-body">
-                    <h5 class="card-title">
-                        ${data["name"]["common"]}
-                </h5>
-                    <ul>
-                    <ul class="list-group list-group-flush">
-                        <li class="list-group-item"> Population: ${((data["population"]) / 1000000).toFixed(1) }M  </li>
-                        <li class="list-group-item"> Capital: ${data["capital"]}  </li>
-                        <li class="list-group-item"> Languages: ${Object.values(data["languages"])}</li>
-                    </ul>
-                </ul>
-            </div>
-        </div>
-    </div>
-    `;
-    document.querySelector(".container .row").insertAdjacentHTML("beforeend", html);
-}
 
+    for (let country of data ) {
+
+        const html =
+        `
+                <div class="col-3">
+                    <div class="card h-100">
+                        <img src="${country.flags["png"]}" class="card-img-top" alt="">
+                            <div class="card-body">
+                                <h5 class="card-title">
+                                    ${country["name"]["common"]}
+                            </h5>
+                                <ul>
+                                <ul class="list-group list-group-flush">
+                                    <li class="list-group-item"> Population: ${((country["population"]) / 1000000).toFixed(1)}M  </li>
+                                    <li class="list-group-item"> Capital: ${country["capital"]}  </li>
+                                    <li class="list-group-item"> Languages: ${Object.values(country["languages"])}</li>
+                                </ul>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+    `;
+        document.querySelector(".container .row").insertAdjacentHTML("beforeend", html);
+    }
+
+}
 displayCountry("turkey")
-displayCountry("uk")
