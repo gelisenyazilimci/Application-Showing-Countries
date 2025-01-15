396e872e669b94885202f6fcf836f4cdbf5177de
gelisenyazilimci <burbyets@gmail.com>
2025-01-15 08:49:28 +0300

    finish app

--- Index.js
+++ Index.js
@@ -66,9 +66,9 @@ function renderNeighbors(data){
         html += `
                 <div class="col-2 mt-2">
                     <div class="card">
-                        <img src="${country.flags.png}" class="card-img-top">
+                        <img src="${country.flags["png"]}" class="card-img-top" alt="">
                         <div class="card-body">
-                            <h6 class="card-title">${country.name.common}</h6>
+                            <h6 class="card-title">${country.name["common"]}</h6>
                         </div>
                     </div>
                 </div>
