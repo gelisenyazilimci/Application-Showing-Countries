21cf375a887fd23954193c2e475c2ef60eaef207
gelisenyazilimci <burbyets@gmail.com>
2025-01-15 08:46:40 +0300

    finsh

--- Index.js
+++ Index.js
@@ -1,3 +1,9 @@
+document.querySelector("#btnSearch").addEventListener("click", () => {
+    let text = document.querySelector("#txtSearch").value;
+    displayCountry(text);
+});
+
+
 function displayCountry (country) {
     const request = new XMLHttpRequest();
     request.open('GET', `https://restcountries.com/v3.1/name/${country}`, true);
@@ -8,7 +14,8 @@ function displayCountry (country) {
     request.addEventListener('load', function () {
         // this.responseText --> Metin olarak görüntülenmesini sağlar.
         const data = JSON.parse(request.responseText); // Gelen JSON veri tipini Javascript'in anlayacağı bir hale çevirir.
-        setCountry(data);
+        console.log(data)
+        renderCountry(data[0]);
 
         // komsu country'ler
         const neighborsRequest = new XMLHttpRequest();
@@ -17,36 +24,56 @@ function displayCountry (country) {
         neighborsRequest.send();
         neighborsRequest.addEventListener('load', function () {
             const data = JSON.parse(neighborsRequest.responseText);
-            setCountry(data);
+            renderNeighbors(data);
         });
     });
 }
-function setCountry(data) {
+function renderCountry(data) {
+    document.querySelector("#country-details").innerHTML = `
+        <div class="card mb-3" id="country-details">
+        <div class="card-header">
+            Arama Sonucu
+        </div>
+        <div class="card-body">
+            <div class="row">
+                <div class="col-4"> <img src="${data.flags["png"]}" class="card-img-top" alt="" class="img-fluids"></div>
+                <div class="col-8">
+                    <h3 class="card-title">${data["name"]["common"]}</h3>
+                    <hr>
+                    <div class="row">
+                        <div class="col-4">Nüfus:</div>
+                        <div class="col-8">${((data["population"]) / 1000000).toFixed(1)}M</div>
+                    </div>
+                    <div class="row">
+                        <div class="col-4">Resmi Dili:</div>
+                        <div class="col-8">${data["languages"] ? Object.values(data["languages"]).join(", ") : "Bilgi yok"}</div>
+                    </div>
+                    <div class="row">
+                        <div class="col-4">Baş Kenti:</div>
+                        <div class="col-8">${data["capital"] ? data["capital"][0] : "Bilgi yok"}</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+        `;
+}
 
-    for (let country of data ) {
 
-        const html =
-        `
-                <div class="col-3">
-                    <div class="card h-100">
-                        <img src="${country.flags["png"]}" class="card-img-top" alt="">
-                            <div class="card-body">
-                                <h5 class="card-title">
-                                    ${country["name"]["common"]}
-                            </h5>
-                                <ul>
-                                <ul class="list-group list-group-flush">
-                                    <li class="list-group-item"> Population: ${((country["population"]) / 1000000).toFixed(1)}M  </li>
-                                    <li class="list-group-item"> Capital: ${country["capital"]}  </li>
-                                    <li class="list-group-item"> Languages: ${Object.values(country["languages"])}</li>
-                                </ul>
-                            </ul>
+function renderNeighbors(data){
+    let html = "";
+    for(let country of data) {
+        html += `
+                <div class="col-2 mt-2">
+                    <div class="card">
+                        <img src="${country.flags.png}" class="card-img-top">
+                        <div class="card-body">
+                            <h6 class="card-title">${country.name.common}</h6>
                         </div>
                     </div>
                 </div>
-    `;
-        document.querySelector(".container .row").insertAdjacentHTML("beforeend", html);
-    }
+            `;
 
+    }
+    document.querySelector("#neighbors").innerHTML =  html;
 }
-displayCountry("turkey")
